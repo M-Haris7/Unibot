6d4fc277608cac10787584367960eb5a8ef51640
Mohammad Haris <91845668+M-Haris7@users.noreply.github.com>
2025-09-10 12:46:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Unibot",
+    version="0.1.0",
+    author="Haris",
+    author_email="thatitguy962@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
