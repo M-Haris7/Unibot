e61b3c9fd84391c2c08cdc2be15d4fef2774c744
Mohammad Haris <91845668+M-Haris7@users.noreply.github.com>
2025-09-11 11:33:35 +0530

    added vectorstore

---
+++ src/helper.py
@@ -0,0 +1,48 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+# Extract text from pdf file
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data, # path
+        glob="*.pdf", # loads all the file with .pdf extension
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+# Just taking source and page content
+def filter_to_docs_minimal(docs: List[Document]) -> List[Document]:
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content = doc.page_content,
+                metadata = {"source": src}
+            )
+        )
+    return minimal_docs
+
+# Split the dcocuments into chunks
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 150,
+        chunk_overlap = 50
+    )
+    texts_chunk = text_splitter.split_documents(minimal_docs)
+    return texts_chunk
+
+# Create Embeddings
+def download_embeddings():
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name = model_name
+    )
+    return embeddings
+
+embedding = download_embeddings()
---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+"""   
+You are a helpful assistant for Uniworld Studios.
+Your job is to answer the user's question **only** using the information provided in the context below.
+
+{context}
+"""
+)
