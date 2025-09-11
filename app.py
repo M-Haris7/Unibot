e8fa87bd56ae62c49baf72834782b418a1d94bce
Mohammad Haris <91845668+M-Haris7@users.noreply.github.com>
2025-09-11 12:21:50 +0530

    Added chat interface and backend code

--- app.py
+++ app.py
@@ -9,12 +9,54 @@ from dotenv import load_dotenv
 from src.prompt import *
 import os
 
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+embeddings = download_embeddings()
+
+index_name = "unibot"
+
+# using existing index since we have already created it in vector_store.py
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chatModel = ChatGroq(model="openai/gpt-oss-120b")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}")
+    ]
+)
+
+QnA_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, QnA_chain)
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template("chat.html")
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 8080, debug= True)
